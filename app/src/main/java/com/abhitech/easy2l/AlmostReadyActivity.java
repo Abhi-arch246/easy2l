51f9cb68648c80e51599bf7480c2857eeb8b076b
sathyaprakash195 <sathyaprakash195@gmail.com>
2020-03-14 22:24:42 +0530

    aSSAM

--- app/src/main/java/com/abhitech/easy2l/AlmostReadyActivity.java
+++ app/src/main/java/com/abhitech/easy2l/AlmostReadyActivity.java
@@ -37,7 +37,7 @@ public class AlmostReadyActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent1=new Intent(AlmostReadyActivity.this,MainActivity.class);
-                startActivity(intent);
+                startActivity(intent1);
             }
         });
     }
